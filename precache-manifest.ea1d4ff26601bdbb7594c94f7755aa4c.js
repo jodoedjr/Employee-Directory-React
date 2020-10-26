95993e86a45aad999b01e0f984d60a7096c2a500
John Edwards <jdedwards6789@gmail.com>
2020-10-25 20:51:52 -0400

    commit of unbuilt app to main branch

---
+++ precache-manifest.ea1d4ff26601bdbb7594c94f7755aa4c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3314555a3ec99eff916250c7dc6a5284",
+    "url": "/Employee-Directory-React/index.html"
+  },
+  {
+    "revision": "f42a3612fdef21a417a6",
+    "url": "/Employee-Directory-React/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "a6230f5a8ea732e88d60",
+    "url": "/Employee-Directory-React/static/css/main.e9bfdefb.chunk.css"
+  },
+  {
+    "revision": "f42a3612fdef21a417a6",
+    "url": "/Employee-Directory-React/static/js/2.6729d5d5.chunk.js"
+  },
+  {
+    "revision": "6bbb3a8e7360a6a98672414629b231d4",
+    "url": "/Employee-Directory-React/static/js/2.6729d5d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6230f5a8ea732e88d60",
+    "url": "/Employee-Directory-React/static/js/main.021f8001.chunk.js"
+  },
+  {
+    "revision": "141017d80b3497c403f9",
+    "url": "/Employee-Directory-React/static/js/runtime-main.cc6eb4f9.js"
+  }
+]);
